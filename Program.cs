79f4cf96cb95d3d11b6b28633d9a1bbf6f931b0b
nazliozbek <nazlii.ozbek@gmail.com>
2022-04-01 13:22:21 +0300

    ignore eklendi

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace tip_donusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // implicit conversion (bilincsiz donusum)
+
+            byte a =5;
+            sbyte b=30;
+            short c=10;
+
+            int d= a+b+c;
+            Console.WriteLine("d:" +d);
+
+            long h=d;
+            Console.WriteLine("h:"+h);
+
+            float i=h;
+            Console.WriteLine("i:" +i);
+
+            string e="David";
+            char f='k';
+            object g= e+f+d;
+            Console.WriteLine("g:"+g);
+
+
+            // explicit conversion (bilincli donusum)
+
+            Console.WriteLine("..... explicit conversion.....");
+            int x=4;
+            byte y=(byte)x;
+            Console.WriteLine("y:"+y);
+            
+            int z=100;
+            byte t=(byte)z;
+            Console.WriteLine("z:"+t);
+
+            float w=10.3f;
+            byte v=(byte)w;
+            Console.WriteLine("v:"+v);
+
+            // Tostring methodu
+            Console.WriteLine(".....TOoSrting methodu.....");
+            int xx=6;
+            string yy= xx.ToString();
+            Console.WriteLine("yy:"+yy);
+
+            string zz=12.5f.ToString();
+            Console.WriteLine("zz:"+zz);
+
+            // Sytem.convert
+
+            Console.WriteLine(".....System.Convert sinifi .....");
+            string s1="10",s2="20";
+            int sayi1,sayi2;
+            int toplam;
+
+            sayi1=Convert.ToInt32(s1);
+            sayi2=Convert.ToInt32(s2);
+            toplam= sayi1+sayi2;
+
+            Console.WriteLine("toplam: "+toplam);
+
+
+            // Parse
+            Console.WriteLine(".....Parse.....");
+            ParseMethod();
+
+        }
+
+        public static void ParseMethod()
+        {
+            string metin1="10";
+            string metin2="10.25";
+            int rakam1;
+            double double1;
+
+            rakam1= Int32.Parse(metin1);
+            Console.WriteLine("rakam1: "+rakam1);
+
+            double1 =Double.Parse(metin2);
+            Console.WriteLine("double1: "+ double1);
+
+
+        }
+    }
+}
